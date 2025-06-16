51aef0f228556859f47ab50dafaf5908ec7c2b88
rhiaji <mrdavepmariano@gmail.com>
2025-06-16 20:01:19 +0800

    loading

--- src/hooks/ProtectedRoute.js
+++ src/hooks/ProtectedRoute.js
@@ -2,12 +2,9 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import Cookies from 'js-cookie'
-import { useApp } from '@/context/AppContext'
-import LoadingPage from '@/components/LoadingPage'
 
 function ProtectedRoute({ children }) {
 	const router = useRouter()
-	const { loading } = useApp()
 
 	useEffect(() => {
 		const auth = Cookies.get('auth_token')
@@ -15,12 +12,7 @@ function ProtectedRoute({ children }) {
 		if (!auth) {
 			router.replace('/')
 		}
-	}, [loading, router])
-
-	// Show loading page while fetching user data
-	if (loading) {
-		return <LoadingPage />
-	}
+	}, [router])
 
 	return <>{children}</>
 }
